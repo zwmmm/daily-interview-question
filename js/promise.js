68cbcbc75a337ff866b572d0006daf2b2f2f4c6a
zhengwei <23490029@qq.com>
2019-07-10 14:42:59 +0800

    基础版promise

--- js/promise.js
+++ js/promise.js
@@ -11,6 +11,10 @@ function MyPromise(fn) {
     this.resolveValue = undefined;
     this.rejectValue = undefined;
     this.status = PENDING; // promise 的状态
+
+    // 为什么需要数组？
+    // 因为同一个promise是可以多次调用then的，注意这里的多次并不是链式调用。
+    // 例如：promise.then() promise.then() 这样去调用两次
     this.rejectCallback = []; // 失败的回调函数集合
     this.resolveCallback = []; // 成功的回调函数集合
 
@@ -42,24 +46,37 @@ function MyPromise(fn) {
     }
 }
 
-MyPromise.prototype.then = function(resolve, reject) {
+function resolutionProcedure(promise, value, resolve, reject) {
+    if (promise === value) { return reject(new TypeError('then不能返回this')) };
+    if (value instanceof MyPromise) { return value.then(resolve, reject) };
+    resolve(value); // 继续触发下一个then
+};
+
+MyPromise.prototype.then = function(onResolve = () => {}, onReject = () => {}) {
+    let nextPromise; // then可以链式调用，所以必须返回一个promise
+
     // 如果promise还处于pending状态，则收集相关的回调
     if (this.status === PENDING) {
-        this.rejectCallback.push(reject);
-        this.resolveCallback.push(resolve);
+        return nextPromise = new MyPromise((resolve, reject) => {
+            this.rejectCallback.push(value => {
+                resolutionProcedure(nextPromise, onReject(value), resolve, reject);
+            });
+            this.resolveCallback.push(value => {
+                resolutionProcedure(nextPromise, onResolve(value), resolve, reject);
+            });
+        })
     }
 
+    // 不为 pending 状态则直接触发回调 当在setTimeOut等宏任务中调用then方法，就会有可能 status不为 pending
     if (this.status === RESOLVE) {
-
+        return nextPromise = new MyPromise((resolve, reject) => {
+            resolutionProcedure(nextPromise, onResolve(value), resolve, reject);
+        })
     }
 
     if (this.status === REJECT) {
-
+        return nextPromise = new MyPromise((resolve, reject) => {
+            resolutionProcedure(nextPromise, onReject(value), resolve, reject);
+        })
     }
 }
-
-const test = new MyPromise((resolve) => {
-    resolve(1);
-})
-
-test.then(res => console.log(res));
