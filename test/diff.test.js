9d353cf11724dd9ddf243fda8a2b972a6ff67e55
zhengwei <23490029@qq.com>
2019-07-23 16:33:26 +0800

    2019-7-23

--- test/diff.test.js
+++ test/diff.test.js
@@ -2,8 +2,24 @@ import assert from 'assert'
 import diff from '../algorithm/diff'
 
 describe('diff 算法', function() {
-    it('should ', function() {
+    it('当JSON变短的时候直接替换', function() {
         const ret = diff({ b: 1}, { a: 1, c: 2 })
+        assert(JSON.stringify(ret) === JSON.stringify({ b: 1 }))
+    });
+
+    it('修改某个值', function() {
+        const ret = diff({ a: 1, c: 1}, { a: 1, c: 2 })
+        assert(JSON.stringify(ret) === JSON.stringify({ c: 1 }))
+    });
+
+    it('增加一个值', function() {
+        const ret = diff({ a: 1, c: 1, b: 1}, { a: 1, c: 2 })
+        assert(JSON.stringify(ret) === JSON.stringify({ c: 1, b: 1 }))
+    });
+
+    it('增加一个值并删除一个值', function() {
+        const ret = diff({ c: 1, b: 1}, { a: 1, c: 2 })
         console.log(ret)
+        assert(JSON.stringify(ret) === JSON.stringify({ c: 1, b: 1, a: null }))
     });
 })
